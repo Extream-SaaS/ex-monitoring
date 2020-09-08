236fa23fb84e7bd08ceab0d5642fe5c7b4a437e8
Connor Clooney <clooney3@live.co.uk>
2020-09-09 00:31:29 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+const Firestore = require('@google-cloud/firestore');
+const projectId = process.env.GCLOUD_PROJECT_ID;
+
+exports.processMonitoringMessage = async (message, context) => {
+  try {
+    const decodedMessage = message.data ? Buffer.from(message.data, 'base64').toString() : null;
+
+    const db = new Firestore({
+      projectId,
+    });
+
+    const docRef = db.collection('monitoring').doc();
+
+    await docRef.set({
+      ...decodedMessage,
+      addedAt: Firestore.FieldValue.serverTimestamp()
+    });
+    return Promise.resolve();
+  } catch (e) {
+    console.error(e);
+    return Promise.reject(e);
+  }
+};
