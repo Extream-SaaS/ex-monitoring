53b73934dc7df4b56c723a5abdbc3dd220e6e62f
Connor Clooney <clooney3@live.co.uk>
2020-10-01 21:25:37 +0100

    Auth header for processor.

--- processor/index.js
+++ processor/index.js
@@ -1,9 +1,13 @@
 'use strict';
 // const Firestore = require('@google-cloud/firestore');
-// const projectId = process.env.GCLOUD_PROJECT_ID;
+const projectId = process.env.GCLOUD_PROJECT_ID;
+const authorizationHeader = process.env.AUTHORIZATION_HEADER;
 
-exports.processMessages = async () => {
+exports.processMessages = async (req, res) => {
   try {
+    if (!req.headers.authorization || req.headers.authorization !== authorizationHeader) {
+      return res.sendStatus(401);
+    }
   console.log('hello');
     return Promise.resolve();
   } catch (e) {
