7b4685c185ccab82306f538b8da376ce865bf7dc
Connor Clooney <clooney3@live.co.uk>
2020-10-11 02:55:19 +0100

    Event tracking processor

---
+++ event-tracking/processor/index.js
@@ -0,0 +1,37 @@
+'use strict';
+const Firestore = require('@google-cloud/firestore');
+const projectId = process.env.GCLOUD_PROJECT_ID;
+const environment = process.env.ENVIRONMENT || null;
+
+const config = {
+    projectId,
+};
+if (!environment) {
+    config.keyFilename = './keyfile.json';
+}
+
+const db = new Firestore(config);
+
+exports.processEventTrackingMessage = (event) => {
+    try {
+        console.log('event', event.value);
+        console.log('resource', event.resource);
+        return Promise.resolve();
+    } catch (e) {
+        console.error(e);
+        return Promise.reject(e);
+    }
+};
+
+// exports.processEventTrackingMessage = functions.firestore
+//     .document('monitoring-event-tracking/{document}')
+//     .onCreate((snapshot, context) => {
+//         try {
+//             const data = snapshot.data();
+//             console.log('data', data);
+//             return Promise.resolve();
+//         } catch (e) {
+//             console.error(e);
+//             return Promise.reject(e);
+//         }
+//     });
