faccdccb2d89b0e069bb84c4b158aab3f3453408
Connor Clooney <clooney3@live.co.uk>
2020-10-11 22:45:25 +0100

    env vars for adobe sender.

--- event-tracking/sender/adobe/index.js
+++ event-tracking/sender/adobe/index.js
@@ -1,14 +1,30 @@
 'use strict';
+const {PubSub} = require('@google-cloud/pubsub');
+const grpc = require('grpc');
+const axios = require('axios');
+const projectId = process.env.GCLOUD_PROJECT_ID;
+const adobeEndpoint = process.env.ADOBE_ENDPOINT;
+const pubsub = new PubSub({grpc, projectId});
 
 exports.sendEventTrackingMessage = async (message) => {
     try {
         const decodedMessage = message.data ? Buffer.from(message.data, 'base64').toString() : null;
         // const parsed = JSON.parse(decodedMessage);
         console.info('decoded', decodedMessage);
+        await pushToDeadletter(message);
         const error = new Error('broken');
         return Promise.reject(error);
     } catch (e) {
         console.error(e);
         return Promise.reject(e);
+    }
+
+    function sendRequest() {
+
+    }
+
+    async function pushToDeadletter(message) {
+        const dataBuffer = Buffer.from(message);
+        await pubsub.topic('ex-monitoring-dead-letter').publish(dataBuffer);
     }
 };
