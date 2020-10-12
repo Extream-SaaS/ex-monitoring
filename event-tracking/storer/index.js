a09dcd996ff8ba4c891064dd61635f27e1caa0d2
Connor Clooney <clooney3@live.co.uk>
2020-10-12 14:24:52 +0100

    origin

--- event-tracking/storer/index.js
+++ event-tracking/storer/index.js
@@ -3,7 +3,9 @@ const Firestore = require('@google-cloud/firestore');
 const projectId = process.env.GCLOUD_PROJECT_ID;
 const environment = process.env.ENVIRONMENT || null;
 const authorizationHeader = process.env.EVENT_TRACKING_STORER_AUTHORIZATION_HEADER;
-
+const cors = require('cors')({
+    origin: true,
+});
 const config = {
     projectId,
 };
@@ -13,29 +15,31 @@ if (!environment) {
 
 const db = new Firestore(config);
 
-exports.storeEventTrackingMessage = async (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.set('Access-Control-Allow-Credentials', 'true');
-    if (req.method === 'OPTIONS') {
-        res.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
-        res.set('Access-Control-Allow-Headers', 'Authorization', 'Content-Type');
-        res.set('Access-Control-Max-Age', '3600');
-        return res.sendStatus(204);
-    }
+exports.storeEventTrackingMessage = (req, res) => {
+    // res.set('Access-Control-Allow-Origin', '*');
+    // res.set('Access-Control-Allow-Credentials', 'true');
+    // if (req.method === 'OPTIONS') {
+    //     res.set('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    //     res.set('Access-Control-Allow-Headers', 'Authorization', 'Content-Type');
+    //     res.set('Access-Control-Max-Age', '3600');
+    //     return res.sendStatus(204);
+    // }
     // res.set('Access-Control-Allow-Methods', 'POST');
     try {
-        if (!req.headers.authorization || req.headers.authorization !== authorizationHeader) {
-            return res.sendStatus(401);
-        }
-        if (!req.body.eventId) {
-            return res.sendStatus(400);
-        }
-        const docRef = db.collection('monitoring-event-tracking').doc();
-        await docRef.set({
-            ...req.body,
-            addedAt: Firestore.FieldValue.serverTimestamp()
+        return cors(req, res, async () => {
+            if (!req.headers.authorization || req.headers.authorization !== authorizationHeader) {
+                return res.sendStatus(401);
+            }
+            if (!req.body.eventId) {
+                return res.sendStatus(400);
+            }
+            const docRef = db.collection('monitoring-event-tracking').doc();
+            await docRef.set({
+                ...req.body,
+                addedAt: Firestore.FieldValue.serverTimestamp()
+            });
+            return res.sendStatus(200);
         });
-        return res.sendStatus(200);
     } catch (e) {
         console.error(e);
         return res.sendStatus(500);
