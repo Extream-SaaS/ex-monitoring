e0bf3f3fe858aa059dd38daa0e3ec26a7ce6d277
Connor Clooney <clooney3@live.co.uk>
2020-10-12 10:55:17 +0100

    set timestamp on processed.

--- event-tracking/processor/index.js
+++ event-tracking/processor/index.js
@@ -22,7 +22,7 @@ exports.processEventTrackingMessage = async (event) => {
         const doc = await docRef.get();
         const docData = doc.data();
         await pushToQueue(docData.payload);
-        docData.processed = true;
+        docData.processed = Firestore.TimeStamp.now();
         await docRef.set(docData);
         return Promise.resolve();
     } catch (e) {
