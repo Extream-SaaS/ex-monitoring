2727216249e2f2be638614a0813d42734c9f7567
Connor Clooney <clooney3@live.co.uk>
2020-10-12 16:26:20 +0100

    new env var for fetcher. Switched to query param

--- event-tracking/fetcher/index.js
+++ event-tracking/fetcher/index.js
@@ -2,8 +2,10 @@
 const Firestore = require('@google-cloud/firestore');
 const projectId = process.env.GCLOUD_PROJECT_ID;
 const environment = process.env.ENVIRONMENT || null;
-const authorizationHeader = process.env.AUTHORIZATION_HEADER;
-
+const authorizationHeader = process.env.EVENT_TRACKING_FETCHER_AUTHORIZATION_HEADER;
+const cors = require('cors')({
+    origin: true,
+});
 const config = {
     projectId,
 };
@@ -15,28 +17,31 @@ const db = new Firestore(config);
 
 exports.fetchEventTrackingMessages = async (req, res) => {
     try {
-        if (!req.headers.authorization || req.headers.authorization !== authorizationHeader) {
-            return res.sendStatus(401);
-        }
-        if (!req.body.eventId) {
-            return res.sendStatus(400);
-        }
-        const collectionRef = db.collection('monitoring-event-tracking');
-        const snapshot = await collectionRef.where('eventId', '==', req.body.eventId).orderBy('addedAt', 'desc').get();
+        return cors(req, res, async () => {
+            if (!req.headers.authorization || req.headers.authorization !== authorizationHeader) {
+                return res.sendStatus(401);
+            }
+            if (!req.query.eventId) {
+                return res.sendStatus(400);
+            }
+            const collectionRef = db.collection('monitoring-event-tracking');
+            console.info(`fetching for eventId: ${req.query.eventId}`);
+            const snapshot = await collectionRef.where('eventId', '==', req.query.eventId).orderBy('addedAt', 'desc').get();
 
-        if (snapshot.empty) {
-            console.log('No matching documents.');
-            return res.json({});
-        }
-        const docs = snapshot.docs.map((doc) => {
-            const data = doc.data();
-            data.addedAt = data.addedAt.toMillis();
-            return {
-                id: doc.id,
-                ...data,
-            };
+            if (snapshot.empty) {
+                console.log('No matching documents.');
+                return res.json({});
+            }
+            const docs = snapshot.docs.map((doc) => {
+                const data = doc.data();
+                data.addedAt = data.addedAt.toMillis();
+                return {
+                    id: doc.id,
+                    ...data,
+                };
+            });
+            return res.json(docs);
         });
-        return res.json(docs);
     } catch (e) {
         console.error(e);
         return res.sendStatus(500);
